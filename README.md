2aaf619f28a369606ab15dceaa9340eae1cfd4b5
Colin Mahns <goatman93@gmail.com>
2015-09-22 00:33:51 -0400

    stopping for the night, will continue tomorrow forgot to git mv some files, oh well

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
-tor-relay-bootstrap
+tor-bootstrap
 ===================
 
-This is a script to bootstrap a Debian server to be a set-and-forget Tor relay. I've only tested it in Wheezy, but it should work on any modern Debian or Ubuntu version. Pull requests are welcome.
+This is a script to bootstrap a Debian server to be a set-and-forget Tor relay, bridge, or exit. This script is meant for a Debian Jessie host.
+
+Pull requests are welcome.
 
 tor-relay-bootstrap does this:
 
 * Upgrades all the software on the system
 * Adds the deb.torproject.org repository to apt, so Tor updates will come directly from the Tor Project
-* Installs and configures Tor to be a relay (but still requires you to manually edit torrc to set Nickname, ContactInfo, etc. for this relay)
+* Installs and configures Tor to become a relay, bridge, or exit based off of the corresponding flag that is passed to the program. This still requires you to manually edit torrc to set Nickname, ContactInfo, etc. for this relay.
 * Configures sane default firewall rules
 * Configures automatic updates
 * Installs tlsdate to ensure time is synced
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,50 @@
 #!/bin/bash
 
+usage(){
+    echo "bootstrap.sh [options]"
+    echo "  -b          Set up a obsf4 Tor bridge"
+    echo "  -r          Set up a (non-exit) Tor relay"
+    echo "  -x          Set up a Tor exit relay"
+    exit -1
+}
+
+# pretty colors
+GREEN='\e0;32m'
+RED='\e[0;31'
+PURPLE='\e[0;35'
+NC='\e[0m'
+
+# Process options
+unset TYPE
+while getopts "br" option; do
+  case $option in
+    b ) [ -n "$TYPE" ] && usage ; TYPE="bridge" ;;
+    r ) [ -n "$TYPE" ] && usage ; TYPE="relay" ;;
+    x ) [ -n "$TYPE" ] && usage ; TYPE="exit" ;;
+    * ) usage ;;
+    esac
+done
+
 # check for root
 if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root" 1>&2
+    echo -e "${RED}This script must be run as root${NC}" 1>&2
     exit 1
 fi
 
 PWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # update software
-echo "== Updating software"
+echo -e "${PURPLE}== Updating software"
 apt-get update
 apt-get dist-upgrade -y
 
+# apt-transport-https allows https debian mirrors. it's more fun that way.
+# https://guardianproject.info/2014/10/16/reducing-metadata-leakage-from-software-updates/
+# granted it doesn't fix *all* metadata problems
+# see https://labs.riseup.net/code/issues/8143 for more on this discussion
 apt-get install -y lsb-release apt-transport-https
 
-# add official Tor repository
+# add official Tor repository w/ https
 if ! grep -q "https://deb.torproject.org/torproject.org" /etc/apt/sources.list; then
     echo "== Adding the official Tor repository"
     echo "deb https://deb.torproject.org/torproject.org `lsb_release -cs` main" >> /etc/apt/sources.list
@@ -26,11 +55,22 @@ fi
 
 # install tor and related packages
 echo "== Installing Tor and related packages"
-apt-get install -y deb.torproject.org-keyring tor tor-arm tor-geoipdb
+if [[ "$TYPE" == "relay" ]] ||  [[ "$TYPE" == "exit" ]] ; then
+    apt-get install -y deb.torproject.org-keyring tor tor-arm tor-geoipdb
+elif [ "$TYPE" == "bridge" ] ; then
+    apt-get install -y deb.torproject.org-keyring tor tor-arm tor-geoipdb obfsproxy golang libcap2-bin
+    go get git.torproject.org/pluggable-transports/obfs4.git/obfs4proxy
+fi
 service tor stop
 
 # configure tor
-cp $PWD/etc/tor/torrc /etc/tor/torrc
+if [ "$TYPE" == "relay" ] ; then
+    cp $PWD/etc/tor/relaytorrc /etc/tor/torrc
+elif [ "$TYPE" == "bridge" ] ; then
+    cp $PWD/etc/tor/bridgetorrc /etc/tor/torrc
+elif [ "$TYPE" == "exit" ] ; then
+    cp $PWD/etc/tor/exittorrc /etc/tor/torrc
+fi
 
 # configure firewall rules
 echo "== Configuring firewall rules"
@@ -38,8 +78,16 @@ apt-get install -y debconf-utils
 echo "iptables-persistent iptables-persistent/autosave_v6 boolean true" | debconf-set-selections
 echo "iptables-persistent iptables-persistent/autosave_v4 boolean true" | debconf-set-selections
 apt-get install -y iptables iptables-persistent
-cp $PWD/etc/iptables/rules.v4 /etc/iptables/rules.v4
-cp $PWD/etc/iptables/rules.v6 /etc/iptables/rules.v6
+if [ "$TYPE" == "relay" ] ; then
+    cp $PWD/etc/iptables/relayrules.v4 /etc/iptables/rules.v4
+    cp $PWD/etc/iptables/relayrules.v6 /etc/iptables/rules.v6
+elif [ "$TYPE" == "bridge" ] ; then
+    cp $PWD/etc/iptables/bridgerules.v4 /etc/iptables/rules.v4
+    cp $PWD/etc/iptables/bridgerules.v6 /etc/iptables/rules.v6
+elif [ "$TYPE" == "exit" ] ; then
+    cp $PWD/etc/iptables/exitrules.v4 /etc/iptables/rules.v4
+    cp $PWD/etc/iptables/exitrules.v6 /etc/iptables/rules.v6
+fi
 chmod 600 /etc/iptables/rules.v4
 chmod 600 /etc/iptables/rules.v6
 iptables-restore < /etc/iptables/rules.v4
@@ -59,20 +107,7 @@ sed -i.bak 's/GRUB_CMDLINE_LINUX="\(.*\)"/GRUB_CMDLINE_LINUX="\1 apparmor=1 secu
 update-grub
 
 # install tlsdate
-if [ "$(lsb_release -cs)" == "wheezy" ]; then
-	# tlsdate isn't in wheezy
-	if [ "$((echo 3.5; uname -r) | sort -cV 2>&1)" == "" ]; then
-		# if we have seccomp (>= linux 3.5) we can backport it
-		if ! grep -q "wheezy-backports" /etc/apt/sources.list; then
-			echo "deb http://ftp.debian.org/debian wheezy-backports main" >> /etc/apt/sources.list
-			apt-get update
-		fi
-		apt-get install -y tlsdate
-	fi
-else
-	# later than wheezy
-	apt-get install -y tlsdate
-fi
+apt-get install -y tlsdate
 
 # install monit
 apt-get install -y monit
@@ -82,7 +117,7 @@ service monit restart
 # configure sshd
 ORIG_USER=$(logname)
 if [ -n "$ORIG_USER" ]; then
-	echo "== Configuring sshd"
+	echo -e "== Configuring sshd ${NC}"
 	# only allow the current user to SSH in
 	echo "AllowUsers $ORIG_USER" >> /etc/ssh/sshd_config
 	echo "  - SSH login restricted to user: $ORIG_USER"
@@ -96,16 +131,16 @@ if [ -n "$ORIG_USER" ]; then
 		fi
 	else
 		# user logged in with a password rather than keys
-		echo "  - You do not appear to be using SSH key authentication.  You should set this up manually now."
+		echo -e "${RED}  - You do not appear to be using SSH key authentication.  You should set this up manually now.${NC}"
 	fi
 	service ssh reload
 else
-	echo "== Could not configure sshd automatically.  You will need to do this manually."
+	echo -e "${RED}== Could not configure sshd automatically.  You will need to do this manually.${NC}"
 fi
 
 # final instructions
 echo ""
-echo "== Try SSHing into this server again in a new window, to confirm the firewall isn't broken"
+echo -e "${GREEN}== Try SSHing into this server again in a new window, to confirm the firewall isn't broken"
 echo ""
 echo "== Edit /etc/tor/torrc"
 echo "  - Set Address, Nickname, Contact Info, and MyFamily for your Tor relay"
@@ -119,4 +154,4 @@ echo "== Consider having /etc/apt/sources.list update over HTTPS and/or HTTPS+To
 echo "   see https://guardianproject.info/2014/10/16/reducing-metadata-leakage-from-software-updates/"
 echo "   for more details"
 echo ""
-echo "== REBOOT THIS SERVER"
+echo -e "== REBOOT THIS SERVER${NC}"
